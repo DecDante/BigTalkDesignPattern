2ebc754fea385016546f8fea39e64878e73c4276
Bruce Kwok <43959116+DecDante@users.noreply.github.com>
2022-12-23 14:48:56 +0800

    Delete v1.0 directory

--- v1.0/ConcreteStateA.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "State.h"
-
-class ConcreteStateA : public State 
-{
-public:
-  virtual void Handle(Context* context);
-};
--- v1.0/ConcreteStateB.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "State.h"
-
-class ConcreteStateB : public State
-{
-public:
-  virtual void Handle(Context* context);
-};
